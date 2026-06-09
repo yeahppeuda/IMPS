62cd3e9a5d272b4184d0cf4a2da7f0aca75ba4b0
Justine Kenneth <dyaastineibo@gmail.com>
2026-06-09 16:27:07 +0800

    Add files via upload

---
+++ User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "staff"],
+      default: "staff",
+      required: true
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
